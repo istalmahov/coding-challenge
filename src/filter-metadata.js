dc06359bc4667d88f338e1389e153fc2eff53fd5
Ilya Stalmahov <i.stalmahov@dunice.net>
2022-07-08 09:18:18 +0300

    implemented filterMetadata and getMetadata functions

--- src/filter-metadata.js
+++ src/filter-metadata.js
@@ -1,13 +1,68 @@
-/*
-type Metadata = {
-  url: string | null;
-  siteName: string | null;
-  title: string | null;
-  description: string | null;
-  keywords: string[] | null;
-  author: string | null;
+/**
+ * @typedef Metadata
+ * @type {object}
+ * @property {string} url
+ * @property {string} siteName
+ * @property {string} title
+ * @property {string} description
+ * @property {string[]} keywords
+ * @property {string} author
+ */
+
+const SPECIAL_CHARS_REGEXP = /[@#$%^&*_=+.,-]/g;
+
+/**
+ * Splits a string into an array of strings if special characters are present.
+ * light-year => [light, year, lightyear, light-year]
+ * @param {string} word - The search query string
+ */
+const handleSpecialChars = (word) => {
+  if (!word.match(SPECIAL_CHARS_REGEXP)) return [word];
+
+  const splittedBySpecialChars = word.split(SPECIAL_CHARS_REGEXP);
+  const specialCharsRemoved = word.replace(SPECIAL_CHARS_REGEXP, "");
+
+  return [...splittedBySpecialChars, specialCharsRemoved, word];
 };
-*/
+
+/**
+ * Splits query string into an array of strings
+ * @param {string} query - The search query string
+ * @returns {string[]}
+ */
+const getQueryArray = (query) => {
+  return query
+    .split(" ")
+    .reduce((words, currentWord) => {
+      words.push(...handleSpecialChars(currentWord));
+      return words;
+    }, [])
+    .filter((word) => word.length > 1);
+};
+
+/**
+ * Combines metadata values into one string and removes special characters
+ * @param {Metadata} metadata - A Metadata object
+ * @returns {string}
+ */
+const formatMetadataValues = (metadata) => {
+  return Object.values(metadata)
+    .join(" ")
+    .replace(SPECIAL_CHARS_REGEXP, "")
+    .toLowerCase();
+};
+
+/**
+ * Checks if metadata matches a query
+ * @param {Metadata} metadata - A Metadata object
+ * @param {string[]} query - The search query string
+ * @returns {boolean}
+ */
+const doesMetadataMatch = (metadata, query) => {
+  const values = formatMetadataValues(metadata);
+
+  return query.some((word) => values.includes(word.toLowerCase()));
+};
 
 /**
  * Filters the given Metadata array to only include the objects that match the given search query.
@@ -22,6 +77,12 @@ type Metadata = {
  * @returns {Metadata[]} - An array of Metadata objects that match the given search query
  */
 export default function filterMetadata(metadata, query) {
-  // TODO: delete and replace this with your code
-  return metadata;
+  if (!metadata || !Array.isArray(metadata) || typeof query !== "string")
+    return [];
+
+  if (!query) return metadata;
+
+  const queryArray = getQueryArray(query);
+
+  return metadata.filter((m) => doesMetadataMatch(m, queryArray));
 }
--- src/get-metadata.js
+++ src/get-metadata.js
@@ -1,14 +1,38 @@
-// Note: Please do not use JSDOM or any other external library/package (sorry)
-/*
-type Metadata = {
-  url: string;
-  siteName: string;
-  title: string;
-  description: string;
-  keywords: string[];
-  author: string;
+/**
+ * @typedef Metadata
+ * @type {object}
+ * @property {string} url
+ * @property {string} siteName
+ * @property {string} title
+ * @property {string} description
+ * @property {string[]} keywords
+ * @property {string} author
+ */
+
+const NODES_REG_EXPS = {
+  head: /<head>(.*?)<\/head>/i,
+  title: /<title>(.*?)<\/title>/i,
+  url: /<meta\s*property="og:url"\s*content="(.*?)"\s*\/?>/i,
+  siteName: /<meta\s*property="og:site_name"\s*content="(.*?)"\s*\/?>/i,
+  description: /<meta\s*property="og:description"\s*content="(.*?)"\s*\/?>/i,
+  description2: /<meta\s*name="description"\s*content="(.*?)"\s*\/?>/i,
+  keywords: /<meta\s*name="keywords"\s*content="(.*?)"\s*\/?>/i,
+  author: /<meta\s*name="author"\s*content="(.*?)"\s*\/?>/i,
 };
-*/
+
+const NEWLINE_REGEXP = /\r?\n|\r/g;
+
+/**
+ * Executes regular expression and returns a catch group
+ * @param {string} html The complete HTML document text
+ * @param {RegExp} node The node to search for. Examples: '<title>', '<meta property="og:url"'
+ * @returns {string} A string, representing a node, or an empty string.
+ */
+const getNodeValue = (html, node) => {
+  const result = node.exec(html);
+
+  return result ? result[1] : null;
+};
 
 /**
  * Gets the URL, site name, title, description, keywords, and author info out of the <head> meta tags from a given html string.
@@ -19,17 +43,35 @@ type Metadata = {
  * 5. Get the keywords from the <meta name="keywords"> tag and split them into an array.
  * 6. Get the author from the <meta name="author"> tag.
  * If any of the above tags are missing or if the values are empty, then the corresponding value will be null.
- * @param html The complete HTML document text to parse
- * @returns A Metadata object with data from the HTML <head>
+ * @param {string} html The complete HTML document text to parse
+ * @returns {Metadata} A Metadata object with data from the HTML <head>
  */
 export default function getMetadata(html) {
-  // TODO: delete and replace this with your code
+  if (!html || typeof html !== "string")
+    return {
+      url: null,
+      siteName: null,
+      title: null,
+      description: null,
+      keywords: null,
+      author: null,
+    };
+
+  const flatHtml = html.replace(NEWLINE_REGEXP, " ");
+
+  const head = getNodeValue(flatHtml, NODES_REG_EXPS.head);
+
   return {
-    url: "https://www.example.com/something/to-test",
-    siteName: "Example Site Name",
-    title: "Example Title",
-    description: "Example description.",
-    keywords: ["example", "keywords"],
-    author: "Example Author",
+    url: getNodeValue(head, NODES_REG_EXPS.url),
+    siteName: getNodeValue(head, NODES_REG_EXPS.siteName),
+    title: getNodeValue(head, NODES_REG_EXPS.title),
+    description:
+      getNodeValue(head, NODES_REG_EXPS.description) ||
+      getNodeValue(head, NODES_REG_EXPS.description2),
+    keywords:
+      getNodeValue(head, NODES_REG_EXPS.keywords)
+        ?.split(",")
+        .filter((keyword) => !!keyword) || null,
+    author: getNodeValue(head, NODES_REG_EXPS.author),
   };
 }
